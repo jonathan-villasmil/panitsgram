dbeee2ae559adcf4905267c163749ff1e3fd2d51
jonathan <jonathanvillasmildeabreu@gmail.com>
2024-08-28 04:38:48 +0200

    Controllers config

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Services\FileService;
+use Illuminate\Http\Request;
+
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $request->validate([
+            'file' => 'required|mimes:jpg,jpeg,png',
+            'text' => 'required'
+        ]);
+        $post = (new FileService)->updateFile($post, $request, 'post');
+
+        $post->user_id = auth()->user()->id;
+        $post->text = $request->input('text');
+        $post->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+
+        if (!empty($post->file)) {
+            $currentFile = public_path() . $post->file;
+
+            if (file_exists($currentFile)) {
+                unlink($currentFile);
+            }
+        }
+
+        $post->delete();
+    }
+}
